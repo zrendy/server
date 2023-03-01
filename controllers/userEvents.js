baa27aeaa3bad911317270ea9c61db5a5b22a272
zrendy <zrendy@gmail.com>
2023-03-01 12:51:00 -0500

    added comments

--- controllers/userEvents.js
+++ controllers/userEvents.js
@@ -1,7 +1,7 @@
 const {getUserEvents} = require('../models/userEvent_model')
 
 const getUserEventsController = async (req, res) => {
-    //get body 
+    //get the request body
     var username = req.body.username
     const result = await getUserEvents(username);
     res.send(result)
